5af98d7bfc21d0e535ce11e9eb08848a8665c427
Jonathan Mitchell <jonmitchell1234@gmail.com>
2023-07-28 23:31:12 -0400

    fixed url

--- src/bg/background.js
+++ src/bg/background.js
@@ -7,13 +7,13 @@ function sendMessageToContentScript(tabId, message) {
 
 // Retrieve the access token from the specified website
 function getAccessToken() {
-  chrome.tabs.query({ url: 'https://chat.openai.com/*' }, (tabs) => {
-    if (tabs.length > 0) {
-      const tab = tabs[0];
-      sendMessageToContentScript(tab.id, 'getAccessToken');
-    } else {
-      // Handle case where the website is not found in any tabs
-      // ...
-    }
-  });
+	chrome.tabs.query({ url: "https://chat.openai.com/" }, (tabs) => {
+		if (tabs.length > 0) {
+			const tab = tabs[0];
+			sendMessageToContentScript(tab.id, "getAccessToken");
+		} else {
+			// Handle case where the website is not found in any tabs
+			// ...
+		}
+	});
 }
